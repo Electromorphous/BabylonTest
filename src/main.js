c6b6c2521624a123519e102c935d3a9af718a26e
Electromorphous <pstation.474@gmail.com>
2023-09-08 06:34:57 +0530

    updated lot of stuff

--- src/main.js
+++ src/main.js
@@ -1,40 +1,48 @@
 import * as Babylon from "@babylonjs/core";
 import { renderMeshes } from "./render";
 
-let curMesh = null;
+// global variable for hovered mesh
+let selectedMesh = null;
+let prevMaterial = null;
 
-const canvas = document.getElementById("renderCanvas");
+// declare all materials
+let hideFrontFaceMat;
+let quantisedLightMat;
+let fresenelMat;
+let fresnelWithEdgesMat;
 
-const engine = new Babylon.Engine(canvas, true, { stencil: true });
+// defining all outline drawing functions
+const hideFrontFaceFunc = async (mesh) => {
+  // remove shader of previous mesh
+  if (selectedMesh) selectedMesh.material = prevMaterial;
 
-const createScene = () => {
-  const scene = new Babylon.Scene(engine);
-  scene.ambientColor = new Babylon.Color3(1, 1, 1);
-
-  scene.createDefaultCameraOrLight(true, false, true);
-  // scene.createDefaultCamera(true, false, true);
-
-  // render all the meshes
-  renderMeshes(scene);
-
-  // initialise ray casting function for event listener
-  scene.onPointerDown = handleHover;
-
-  return scene;
+  // draw shader on new mesh if it exists
+  if (mesh) {
+    prevMaterial = mesh.material;
+    mesh.material = hideFrontFaceMat;
+    selectedMesh = mesh;
+  } else {
+    selectedMesh = mesh;
+    prevMaterial = mesh;
+  }
 };
 
+let selectedOutlineShader = 1;
+
 const handleHover = () => {
+  // get the mesh which was hovered
   const mesh = getMesh();
-  curMesh ? (curMesh.renderOutline = false) : {};
-  if (mesh) {
-    mesh.renderOutline = true;
-    mesh.outlineColor = new Babylon.Color3(255, 0, 0);
-    mesh.outlineWidth = 0.02;
-    mesh.forceSharedVertices();
-    curMesh = mesh;
-  } else {
-    curMesh = null;
-  }
+
+  console.log(selectedOutlineShader);
+
+  // call whichever outline drawing function is active
+  if (selectedOutlineShader == 1) hideFrontFaceFunc(mesh);
+};
+
+const handleSelectChange = (e) => {
+  const val = e.target.value;
+
+  selectedOutlineShader = val;
 };
 
 const getMesh = () => {
@@ -44,7 +52,36 @@ const getMesh = () => {
   return null;
 };
 
-const scene = createScene();
+// boilerplate Babylon code
+const canvas = document.getElementById("renderCanvas");
+
+const engine = new Babylon.Engine(canvas);
+
+const createScene = async () => {
+  const scene = new Babylon.Scene(engine);
+  scene.ambientColor = new Babylon.Color3(1, 1, 1);
+
+  scene.createDefaultCameraOrLight(true, false, true);
+  // scene.createDefaultCamera(true, false, true);
+
+  // load all materials from Node Material Editor
+  // https://nme.babylonjs.com/#8AKJ1U#1
+  hideFrontFaceMat = await Babylon.NodeMaterial.ParseFromSnippetAsync(
+    "8AKJ1U#2"
+  );
+  hideFrontFaceMat.depthFunction = Babylon.Constants.ALWAYS;
+  hideFrontFaceMat.disableDepthWrite = true;
+  hideFrontFaceMat.alphaMode = Babylon.Constants.ALPHA_ADD;
+
+  renderMeshes(scene);
+
+  // initialise ray casting function for event listener
+  scene.onPointerDown = handleHover;
+
+  return scene;
+};
+
+const scene = await createScene();
 
 engine.runRenderLoop(() => {
   scene.render();
@@ -53,3 +90,6 @@ engine.runRenderLoop(() => {
 window.addEventListener("resize", () => {
   engine.resize();
 });
+
+// setting up GUI function calls
+document.getElementById("outlineType").onchange = handleSelectChange;
